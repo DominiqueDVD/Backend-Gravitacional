ffea4ce6baaf5a8ffbd30604e1eab829e467f5ef
ClMacher <clmacher@gmail.com>
2024-04-19 13:05:58 -0400

    Agregación de rol al usuario

--- schema/user.js
+++ schema/user.js
@@ -10,6 +10,7 @@ const UserSchema = new Mongoose.Schema({
     username: { type: String, required: true},
     email: { type: String, required: true, unique: true},
     password: { type: String, required: true},
+    role: { type: Number, default: 1 },
 });
 
 UserSchema.pre('save', function(next){
